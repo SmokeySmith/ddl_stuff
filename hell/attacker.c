9c318459a9f95544bf85eabcc07f509fbb56dd1a
joe <smokeyroll20@gmail.com>
2025-07-27 18:31:30 +0100

    playing with another idea

--- hell/attacker.c
+++ hell/attacker.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+void someNewFunction() {
+    printf("Some new function all together\n");
+}
+
 void OkMessage() {
     printf("All you programs are belong to us!!!\n");
+    someNewFunction();
 }
